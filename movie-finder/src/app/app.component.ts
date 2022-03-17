40e195c4e71f44a6931f7381e6b7887ac201ea5b
StevenK418 <stevenkelly418@gmail.com>
2022-03-17 11:43:22 +0000

    => Registered the HTTP service.

--- movie-finder/src/app/app.component.ts
+++ movie-finder/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { OmdbApiService } from './services/omdb-api.service';
+import { IOMDBResponse } from './omdbresponse';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [OmdbApiService]
 })
 export class AppComponent {
   title = 'movie-finder';
